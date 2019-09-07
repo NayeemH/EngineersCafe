a14544b9817cab0db2bbafc94c0d2bd07839ebfe
NayeemH <nayeemh67@gmail.com>
2019-09-08 02:04:09 +0600

    updated pop up modal done

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
-const openModalButtons = document.querySelectorAll(['data-model-target'])
-const openModalButtons = document.querySelectorAll(['data-model-close'])
-const ovelay = document.getElementById('overlay')
 
-openModalButtons.forEach(button => {
-    button.addEventListener('click',() =>{
-        const modal = document.querySelectorAll(button.dataset.modalTarget)
-        openModalButtons()
-    })
-})
+
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
